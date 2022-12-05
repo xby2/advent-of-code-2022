e08f384b7036afdb210d7e8486f38f617071f05e
Mohammed Hussain <2152584+mhhussain@users.noreply.github.com>
2022-12-05 13:58:54 -0500

    12-5 solutions

---
+++ 12-5/src.js
@@ -0,0 +1,33 @@
+console.log(require('fs').readFileSync('./input.txt', 'utf8').split('\n')
+  .reduce((x, v, i) => {
+    if (v.charAt(0) === '[') {
+      x.map((_, j) => { if (v[4*j+1] != ' ') x[j].push(v[4*j+1]) })
+    }
+    if(v.charAt(0) === '') {
+      x.map((_, j) => x[j].reverse());
+    }
+    if (v.charAt(0) === 'm') {
+      Array(parseInt(v.split(' ')[1])).fill().map((_, k) => x[parseInt(v.split(' ')[5])-1].push(x[parseInt(v.split(' ')[3])-1].pop()))
+    }
+    return x;
+  }, [[],[],[],[],[],[],[],[],[]])
+  .map(v => v[v.length-1])
+);
+
+console.log(require('fs').readFileSync('./input.txt', 'utf8').split('\n')
+  .reduce((x, v, i) => {
+    if (v.charAt(0) === '[') {
+      x.map((_, j) => { if (v[4*j+1] != ' ') x[j].push(v[4*j+1]) })
+    }
+    if(v.charAt(0) === '') {
+      x.map((_, j) => x[j].reverse());
+    }
+    if (v.charAt(0) === 'm') {
+      const temp = [];
+      Array(parseInt(v.split(' ')[1])).fill().map((_, k) => temp.push(x[parseInt(v.split(' ')[3])-1].pop()))
+      Array(parseInt(v.split(' ')[1])).fill().map((_, k) => x[parseInt(v.split(' ')[5])-1].push(temp.pop()))
+    }
+    return x;
+  }, [[],[],[],[],[],[],[],[],[]])
+  .map(v => v[v.length-1])
+);
