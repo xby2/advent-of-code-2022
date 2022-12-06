eeef04aba8393d427cf3bf2b7930aaab7fec42e5
Mohammed Hussain <2152584+mhhussain@users.noreply.github.com>
2022-12-06 10:55:29 -0500

    removed unused var name

--- 12-6/src.js
+++ 12-6/src.js
@@ -1,9 +1,9 @@
 console.log(require('fs').readFileSync('./input.txt', 'utf8').split('')
-  .map((v, i, a) => i >= 4 ? new Set(a.slice(i-4, i)).size === 4 | 0 : 0 )
+  .map((_, i, a) => i >= 4 ? new Set(a.slice(i-4, i)).size === 4 | 0 : 0 )
   .indexOf(1)
 );
 
 console.log(require('fs').readFileSync('./input.txt', 'utf8').split('')
-  .map((v, i, a) => i >= 14 ? new Set(a.slice(i-14, i)).size === 14 | 0 : 0 )
+  .map((_, i, a) => i >= 14 ? new Set(a.slice(i-14, i)).size === 14 | 0 : 0 )
   .indexOf(1)
 );
