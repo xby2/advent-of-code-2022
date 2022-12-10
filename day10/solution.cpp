82f6b9a366999220cae0dca7525d1b33680633b1
Jason Brown <jasonrbr17@gmail.com>
2022-12-10 01:04:09 -0500

    Improve memory

--- day10/solution.cpp
+++ day10/solution.cpp
@@ -2,50 +2,53 @@
 #include <iostream>
 #include <string>
 #include <set>
-#include <vector>
 #include <cmath>
 
 using std::ifstream;
 using std::string;
-using std::vector;
 using std::set;
 using std::cout;
 using std::endl;
+
+
+void do_cycle(int x, int current_cycle, int& signal_strength_sum) {
+    static const set<int> cycle_strength_checks = {20, 60, 100, 140, 180, 220};
+
+    if (cycle_strength_checks.contains(current_cycle)) {
+        signal_strength_sum += current_cycle * x;
+    }
+
+    int pixel_pos = (current_cycle - 1) % 40;
+    if (pixel_pos == 0) {
+        cout << '\n';
+    }
+    if (abs(x - pixel_pos) <= 1) {
+        cout << "#";
+    } else {
+        cout << ".";
+    }
+}
 
 int main() {
     ifstream input("input.txt");
 
     string command;
-    vector<int> after_cycle_change_value;
-    set<int> cycle_strength_checks = {20, 60, 100, 140, 180, 220};
+    int x = 1;
+    int signal_strength_sum = 0;
+    int current_cycle = 1;
 
     while (input >> command) {
-        after_cycle_change_value.push_back(0);
+       do_cycle(x, current_cycle, signal_strength_sum);
 
         if (command == "addx") {
             int value;
             input >> value;
-            after_cycle_change_value.push_back(value);
+            ++current_cycle;
+            do_cycle(x, current_cycle, signal_strength_sum);
+            x += value;
         }
-    }
 
-    int x = 1;
-    int signal_strength_sum = 0;
-
-    for (uint i = 0; i < after_cycle_change_value.size(); ++i) {
-        if (cycle_strength_checks.contains(i + 1)) {
-            signal_strength_sum += (i + 1) * x;
-        }
-        int pixel_pos = i % 40;
-        if (pixel_pos == 0) {
-            cout << '\n';
-        }
-        if (abs(x - pixel_pos) <= 1) {
-            cout << "#";
-        } else {
-            cout << ".";
-        }
-        x += after_cycle_change_value[i];
+        ++current_cycle;
     }
 
     cout << "\n\n" << signal_strength_sum << endl;
